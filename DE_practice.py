50e2bac3309e65ab1430be2bad5b649165e4e5f5
Lin Xu <lllinx@umich.edu>
2018-06-26 19:56:52 -0500

    practice

--- DE_practice.py
+++ DE_practice.py
@@ -36,7 +36,6 @@ output=[2,10,15]
 """
 def common_element(a,b):
 	result=[]
-	low=0
 	for i in a:
 		low,high=0,len(b)-1
 		while low<high:
@@ -95,6 +94,112 @@ def distinct_list(l):
 	return result
 
 print(distinct_list([1,2,2,3,5,7,7,9]))
+
+
+"""
+flat list: giving [1,2,[3,4,[5,6]],7] ==> [1,2,3,4,5,6,7]
+"""
+def flatten(a):
+	if type(a) is list:
+		if len(a)==0:
+			return []
+		return flatten(a[0])+flatten(a[1:])
+	else:
+		return [a]
+
+print(flatten([1,2,[3,4,[5,6]],7]))
+
+"""
+Count substr in string 
+"""
+def count_substr(string, sub):
+	result=0
+	for i in range(len(string)-len(sub)):
+		if string[i:i+len(sub)]==sub:
+			result+=1
+	return result
+
+print(count_substr("ababcool ab co","ab"))
+
+
+def valid_parenthesis(s):
+	stack=[]
+	dic={")":"(","}":"{","]":"["}
+	for i in s:
+		if i in dic.values():
+			stack.append(i)
+		elif i in dic.keys():
+			if not stack or dic[i]!=stack.pop():
+				return False
+	return stack==[]
+
+def shortest_distance(words, word1, word2):
+	distance=len(words)
+	index1,index2=-1,-1
+	for i in range(len(words)):
+		if words[i]==word1:
+			index1=i
+		elif words[i]==word2:
+			index2=i
+		if index1!=-1 and index2!=-1:
+			distance=min(distance,abs(index1-index2))
+	return distance
+
+"""
+find closest distance of two same words in a string
+"""
+def shortest_distance_2(words):
+	dic={}
+	for i in range(len(words)):
+		if i not in dic:
+			dic[i]
+
+
+"string substitution"
+
+# def string_substi(s,s1):
+
+
+
+
+"""
+InputString = "babca", sortedString = "bca". Get the result = "bbcaa" 
+(The characters in the result string should have the same order of the sortedString, 
+and same counts of the InputString)
+"""
+def string_mute(s1,s2):
+	result={}
+	output=""
+	for i in s1:
+		if i not in result:
+			result[i]=1
+		else:
+			result[i]+=1
+	for j in s2:
+		output+=j*result[j]
+	return output
+		
+	
+
+"""
+int[] a = {1,0,0,2,0,3,......1,0,2,3}
+int[] b = {1,1,0,0,0,1,......2,1,0,1}
+1. Find the SUM(a * b) (a and b could have the different size)
+2. How to optimize the arrays so it can get the SUM(a * b) faster
+"""
+def sum_ab(a,b):
+	length=min(len(a),len(b))
+	result=0
+	for i in range(length):
+		result+=a[i]*b[i]
+	return result
+
+
+
+
+
+
+
 
 
 
