9d512f92f1189fc5714725ea5d2fea9579d958ad
Lin Xu <lllinx@umich.edu>
2018-07-07 23:16:50 -0500

    hashmap

--- 560_Subarray_Sum_Equals_K.py
+++ 560_Subarray_Sum_Equals_K.py
@@ -1,3 +1,38 @@
+"""
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input:nums = [1,1,1], k = 2
+Output: 2
+
+Note:
+
+    The length of the array is in range [1, 20,000].
+    The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+"""
+def subarraySum3(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    dic={0:1}
+    sums, count=0, 0
+    for i in range(len(nums)):
+        sums+=nums[i]
+        if (sums-k) in dic.keys():
+            count+=dic[sums-k]
+        if sums in dic.keys():
+            dic[sums]+=1
+        else:
+            dic[sums]=1
+    return count
+
+    
+
+
+
 def subarraySum1(nums, k):
     """
     :type nums: List[int]
@@ -39,11 +74,7 @@ def subarraySum2(nums, k):
         i+=1
     return result
 
-def subarraySum3(nums, k):
-    """
-    :type nums: List[int]
-    :type k: int
-    :rtype: int
-    """
+
+
     
 
