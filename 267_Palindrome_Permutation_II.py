6f5c25daa37b200059e21f5d84ab60b6b11b48ce
Lin Xu <lllinx@umich.edu>
2018-08-17 22:32:14 -0500

    permutation palindrome

--- 267_Palindrome_Permutation_II.py
+++ 267_Palindrome_Permutation_II.py
@@ -14,4 +14,43 @@ Output: []
 """
 
 def generatePalindromes(s):
+	dic={}
+	for s1 in s:
+		if s1 not in dic:
+			dic[s1]=1
+		else:
+			dic[s1]+=1
+	sum_odd=0
+	middle=""
+	half=""
+	for k,v in dic.items():
+		if v%2==1:
+			sum_odd+=1
+			if sum_odd>1:
+				return []
+			middle=k
+		half+=k*(v//2)
+	res=[]
+	used=[False for _ in half]
+	def dfs(s,temp,res):
+		if len(temp)==len(s):
+			res.append(temp+middle+temp[::-1])
+			return
+		for i in range(len(s)):
+			if used[i]:
+				continue
+			elif i>0 and s[i-1]==s[i] and not used[i-1]:
+				continue
+			temp+=s[i]
+			used[i]=True
+			dfs(s,temp,res)
+			temp=temp[:-1]
+			used[i]=False
+		return res
+	dfs(half,"",res)
+	return res
+
+
+
+
 	
