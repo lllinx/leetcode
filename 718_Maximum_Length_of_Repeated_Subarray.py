7561f0fd5e98029727fb954013b6f20e870f8874
Lin Xu <lllinx@umich.edu>
2018-09-15 21:09:07 +0800

    dp

--- 718_Maximum_Length_of_Repeated_Subarray.py
+++ 718_Maximum_Length_of_Repeated_Subarray.py
@@ -17,4 +17,11 @@ Note:
 """
 
 def findLength(A, B):
-	
+	dp=[[0 for j in range(len(B)+1)] for i in range(len(A)+1)]
+	for i in range(len(A)-1,-1,-1):
+		for j in range(len(B)-1,-1,-1):
+			if A[i]==B[j]:
+				dp[i][j]=dp[i+1][j+1]+1
+	return max(max(row) for row in dp)
+
+
