45cfe1128fbdb99c89e9db2119c7cdb10942fe17
Lin Xu <lllinx@umich.edu>
2018-10-10 23:28:57 -0500

    dp

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -13,6 +13,16 @@ Example 2:
 Input: coins = [2], amount = 3
 Output: -1
 """
-
+# bottom up solution
 def coinChange(coins,amount):
+	dp=[amount+1 for _ in range(amount+1)]
+	dp[0]=0
+	for i in range(1,amount+1):
+		for j in range(len(coins)):
+			if i>=coins[j]:
+				dp[i]=min(dp[i],dp[i-coins[j]]+1)
+	if dp[amount]>amount:
+		return -1
+	return dp[amount]
+
 	
