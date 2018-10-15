2a73a2a23ca0955008bc0bbb65a152737cde6abc
Lin Xu <lllinx@umich.edu>
2018-10-14 21:58:14 -0500

    dp

--- 279_Perfect_Squares.py
+++ 279_Perfect_Squares.py
@@ -16,5 +16,23 @@ Explanation: 13 = 4 + 9.
 
 """
 
+# dynamic programming
 def numSquares(n):
-	
+	dp=[n+1 for _ in range(n+1)]
+	dp[0]=0
+
+	for i in range(1,n+1):
+		for j in range(1,n+1):
+			if j*j<=i:
+				dp[i]=min(dp[i],dp[i-j*j]+1)
+			else:
+				break
+	if dp[n]==n+1:
+		return -1
+	return dp[n]
+
+# could also use BFS
+
+
+
+
