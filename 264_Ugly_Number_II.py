2da488061c2458c06e39f554b3f74036a60f6e68
Lin Xu <lllinx@umich.edu>
2018-10-15 22:48:54 -0500

    dp/math

--- 264_Ugly_Number_II.py
+++ 264_Ugly_Number_II.py
@@ -17,4 +17,26 @@ Note:
 """
 
 def nthUglyNumber(n):
+	if n==0:
+		return False
+
+	k=[0 for _ in range(n)]
+	k[0]=1
+	# pointer for tracking the min 
+	t2,t3,t5=0,0,0
+
+	for i in range(1,n):
+		k[i]=min(k[t2]*2,min(k[t3]*3,k[t5]*5))
+		if k[i]==k[t2]*2:
+			t2+=1
+		if k[i]==k[t3]*3:
+			t3+=1
+		if k[i]==k[t5]*5:
+			t5+=1
+	return k[n-1]
+
 	
+
+
+
+
